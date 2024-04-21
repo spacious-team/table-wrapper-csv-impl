a73cf9302eebac6b04d4547d2a6af09d42c9f09b
Vitalii Ananev <vananiev@users.noreply.github.com>
2024-04-21 17:42:27 +0300

    doesn't close external InputStream

---
+++ src/main/java/org/spacious_team/table_wrapper/csv/CloseIgnoringInputStream.java
@@ -0,0 +1,33 @@
+/*
+ * Table Wrapper CSV Impl
+ * Copyright (C) 2024  Spacious Team <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.spacious_team.table_wrapper.csv;
+
+import java.io.FilterInputStream;
+
+class CloseIgnoringInputStream extends FilterInputStream {
+
+    public CloseIgnoringInputStream(java.io.InputStream in) {
+        super(in);
+    }
+
+    @Override
+    public void close() {
+        // do nothing
+    }
+}
